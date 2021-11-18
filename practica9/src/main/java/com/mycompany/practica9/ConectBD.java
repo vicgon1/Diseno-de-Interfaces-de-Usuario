f1de790df268c21fccbf09dcae0fba5a37efd0e3
vicgon1 <59971176+vicgon1@users.noreply.github.com>
2021-11-18 18:36:17 +0000

    Add files via upload

---
+++ practica9/src/main/java/com/mycompany/practica9/ConectBD.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.practica9;
+
+/**
+ *
+ * @author Víctor
+ */
+public class ConectBD {
+    
+}
